313c2862e69387e95afb1cff1d135ac44c08542b
meetransportation <transportationmeet@gmail.com>
2026-01-28 13:55:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
+// Cerrar menú al hacer click
+document.querySelectorAll("#navMenu a").forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
+});
